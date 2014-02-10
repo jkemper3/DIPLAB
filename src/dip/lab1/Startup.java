55f77fede7537b043939c6ba3af4365bf875c417
Justin <Justin@Justin-PC>
2014-02-09 20:23:10 -0600

    2nd commit Lab 1 finished

--- src/dip/lab1/Startup.java
+++ src/dip/lab1/Startup.java
@@ -11,30 +11,19 @@ import java.text.NumberFormat;
 public class Startup {
 
     public static void main(String[] args) {
-        /*
-         * We'll just use this class for testing our ccde.
-         * We'll provide input and get some output...
-         */
 
-        //Low-level modules
-        Employee emp1 = new HourlyEmployee(10.50, 2020);
-        Employee emp2 = new SalariedEmployee(45000, 1250);
-        Employee emp3 = new SalariedEmployee(90000,0);
-
-        // High-level module
+        Employee employee1 = new HourlyEmployee(12,1872);
+        Employee employee2 = new SalariedEmployee(75000);
+         
         HRService hr = new HRService();
-
         // Just utility code to format numbers nice.
         NumberFormat nf = NumberFormat.getCurrencyInstance();
 
         // Test input/output..
         System.out.println("Employee 1 annual compensation: " +
-            nf.format(hr.getAnnualCompensationForEmployee(emp1)));
+            nf.format(hr.getAnnualCompensationForEmployee(employee1)));
         System.out.println("Employee 2 annual compensation: " +
-            nf.format(hr.getAnnualCompensationForEmployee(emp2)));
-        System.out.println("Employee 3 annual compensation: " +
-            nf.format(hr.getAnnualCompensationForEmployee(emp3,
-            HRService.SALARIED_TYPE)));
+            nf.format(hr.getAnnualCompensationForEmployee(employee2)));
         
     }
 
